d5a2f58572c5b849a83a8e41fe82f8efcd094867
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-07-31 14:03:25 -0400

    the map now exposes a function that can be called from the parent to draw a polygon.

--- src/lib/components/map/map.svelte
+++ src/lib/components/map/map.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	let config = {
+	export let config = {
 		map: {
 			interaction: 'dynamic',
 			viewSettings: {
@@ -21,32 +21,28 @@
 		suportedLanguages: ['en']
 	};
 	$: sConfig = JSON.stringify(config);
-	let c;
 
-	async function addPolyline() {
+	export const addPolyline = function () {
+		let polyline = [
+			[-99, 50],
+			[-99, 54],
+			[-92, 54],
+			[-92, 50],
+			[-99, 51]
+		];
 		console.log(cgpv);
 		console.log(cgpv.api.maps);
 		console.log(cgpv.api.map('mapTwo'));
-		cgpv.api.map('mapTwo').layer.vector.addPolyline(
-			[
-              [-99, 50],
-              [-99, 54],
-              [-92, 54],
-              [-92, 50],
-              [-99, 51],
-				],
-			undefined
-		);
-	}
+		cgpv.api.map('mapTwo').layer.vector.addPolyline(polyline, undefined);
+	};
 
 	onMount(async () => {
-		c = cgpv.init();
+		cgpv.init();
 	});
 </script>
 
 <svelte:head>
 	<script src="https://canadian-geospatial-platform.github.io/geoview/public/cgpv-main.js"></script>
 </svelte:head>
-<button on:click={addPolyline}>fdasf</button>
 <!--for now, we pass data config in the html as javascript configuration is more bug prone.-->
 <div id="mapTwo" class="llwp-map" style="height: 100vh;" data-lang="en" data-config={sConfig} />
--- src/routes/[lang]/map-browser/+page.svelte
+++ src/routes/[lang]/map-browser/+page.svelte
@@ -3,6 +3,7 @@
 	import Filters from '$lib/components/filters/filters.svelte';
 	import Map from '$lib/components/map/map.svelte';
 
+	let testMap;
 	onMount(async () => {
 		let response = await fetch(
 			'https://geocore.api.geo.ca/geo?north=69.8698915662856&east=44.6484375&south=46.01222384063236&west=-180&keyword=&lang=fr&min=1&max=10&sort=title'
@@ -10,8 +11,13 @@
 		const jsonResponse = await response.json();
 		console.log(jsonResponse);
 	});
+
+	function testM() {
+		testMap();
+	}
 </script>
 
 <h1>This page will contain the map based search.</h1>
 <Filters />
-<Map />
+<button on:click={testM}>Draw polygon.</button>
+<Map bind:addPolyline={testMap} />
