04c1cb62566ad3ec85afb92c85118ca601440c65
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-08-01 12:11:06 -0400

    organisations filter now always uses english name as key.

--- src/lib/components/filters/organisation.svelte
+++ src/lib/components/filters/organisation.svelte
@@ -3,38 +3,75 @@
 	import { slide } from 'svelte/transition';
 	import Checkbox from './inputs/checkbox.svelte';
 	let collapse = true;
+
 	let organisations = [
-		'Agence Canadienne de Développement économique du Nord',
-		"Agence d'évaluation d'impact du Canada",
-		'Agence de la santé publique du Canada',
-		'Agence spatiale canadienne',
-		'Agriculture et Agroalimentaire Canada',
-		'Commission canadienne de sûreté nucléaire',
-		'Défense nationale',
-		'Élections Canada',
-		'Emploi et Développement social Canada',
-		'Environnement et Changement climatique Canada',
-		"Gouvernement de l'Alberta",
-		"Gouvernement de l'Île-du-Prince-Édouard",
-		"Gouvernement de l'Ontario",
-		'Gouvernement de la Colombie-Britannique',
-		'Gouvernement de la Nouvelle-Écosse',
-		'Gouvernement de la Saskatchewan',
-		'Gouvernement de Terre-Neuve-et-Labrador',
-		'Gouvernement des Territoires du Nord-Ouest',
-		'Gouvernement du Manitoba',
-		'Gouvernement du Nouveau-Brunswick',
-		'Gouvernement du Nunavut',
-		'Gouvernement du Yukon',
-		'Gouvernement et municipalités du Québec',
-		"Moissonneur web de l'Infrastructure canadienne de données géospatiales",
-		'Parcs Canada',
-		'Patrimoine canadien',
-		'Pêches et Océans Canada',
-		'Ressources naturelles Canada',
-		'Services aux Autochtones Canada',
-		'Statistique Canada',
-		'Transports Canada'
+		{
+			'fr-ca': 'Agence Canadienne de Développement économique du Nord',
+			'en-ca': 'Canadian Northern Economic Development Agency'
+		},
+		{
+			'fr-ca': "Agence d'évaluation d'impact du Canada",
+			'en-ca': 'Impact Assessment Agency of Canada'
+		},
+		{ 'fr-ca': 'Agence de la santé publique du Canada', 'en-ca': 'Public Health Agency of Canada' },
+		{ 'fr-ca': 'Agence spatiale canadienne', 'en-ca': 'Canadian Space Agency' },
+		{
+			'fr-ca': 'Agriculture et Agroalimentaire Canada',
+			'en-ca': 'Agriculture and Agri-Food Canada'
+		},
+		{
+			'fr-ca': 'Commission canadienne de sûreté nucléaire',
+			'en-ca': 'Canadian Nuclear Safety Commission'
+		},
+		{ 'fr-ca': 'Défense nationale', 'en-ca': 'National Defence' },
+		{ 'fr-ca': 'Élections Canada', 'en-ca': 'Elections Canada' },
+		{
+			'fr-ca': 'Emploi et Développement social Canada',
+			'en-ca': 'Employment and Social Development Canada'
+		},
+		{
+			'fr-ca': 'Environnement et Changement climatique Canada',
+			'en-ca': 'Environment and Climate Change Canada'
+		},
+		{ 'fr-ca': "Gouvernement de l'Alberta", 'en-ca': 'Government of Alberta' },
+		{
+			'fr-ca': "Gouvernement de l'Île-du-Prince-Édouard",
+			'en-ca': 'Government of Prince Edward Island'
+		},
+		{ 'fr-ca': "Gouvernement de l'Ontario", 'en-ca': 'Government of Ontario' },
+		{
+			'fr-ca': 'Gouvernement de la Colombie-Britannique',
+			'en-ca': 'Government of British Columbia'
+		},
+		{ 'fr-ca': 'Gouvernement de la Nouvelle-Écosse', 'en-ca': 'Government of Nova Scotia' },
+		{ 'fr-ca': 'Gouvernement de la Saskatchewan', 'en-ca': 'Government of Saskatchewan' },
+		{
+			'fr-ca': 'Gouvernement de Terre-Neuve-et-Labrador',
+			'en-ca': 'Government of Newfoundland and Labrador'
+		},
+		{
+			'fr-ca': 'Gouvernement des Territoires du Nord-Ouest',
+			'en-ca': 'Government of Northwest Territories'
+		},
+		{ 'fr-ca': 'Gouvernement du Manitoba', 'en-ca': 'Government of Manitoba' },
+		{ 'fr-ca': 'Gouvernement du Nouveau-Brunswick', 'en-ca': 'Government of New Brunswick' },
+		{ 'fr-ca': 'Gouvernement du Nunavut', 'en-ca': 'Government of Nunavut' },
+		{ 'fr-ca': 'Gouvernement du Yukon', 'en-ca': 'Government of Yukon' },
+		{
+			'fr-ca': 'Gouvernement et municipalités du Québec',
+			'en-ca': 'Government and Municipalities of Québec'
+		},
+		{
+			'fr-ca': "Moissonneur web de l'Infrastructure canadienne de données géospatiales",
+			'en-ca': 'Canadian Geospatial Data Infrastructure Web Harvester'
+		},
+		{ 'fr-ca': 'Parcs Canada', 'en-ca': 'Parks Canada' },
+		{ 'fr-ca': 'Patrimoine canadien', 'en-ca': 'Canadian Heritage' },
+		{ 'fr-ca': 'Pêches et Océans Canada', 'en-ca': 'Fisheries and Oceans Canada' },
+		{ 'fr-ca': 'Ressources naturelles Canada', 'en-ca': 'Natural Resources Canada' },
+		{ 'fr-ca': 'Services aux Autochtones Canada', 'en-ca': 'Indigenous Services Canada' },
+		{ 'fr-ca': 'Statistique Canada', 'en-ca': 'Statistics Canada' },
+		{ 'fr-ca': 'Transports Canada', 'en-ca': 'Transport Canada	' }
 	];
 	function init(key) {
 		return $page.url.searchParams.get(key) == 'on';
@@ -50,7 +87,7 @@
 	<ul transition:slide>
 		{#each organisations as x}
 			<li>
-				<Checkbox id={getId(x)} name={x} />
+				<Checkbox id={getId(x['en-ca'])} name={x['fr-ca']} />
 			</li>
 		{/each}
 	</ul>
