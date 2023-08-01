579cb4e687dfb7bf431a4e919d1406acb602883c
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-08-01 10:34:16 -0400

    wip add filters to query for data.

--- src/routes/[lang]/map-browser/+page.js
+++
@@ -1,9 +0,0 @@
-export const load = async ({ fetch, params }) => {
-	const response = await fetch(
-		'https://geocore.api.geo.ca/geo?north=69.8698915662856&east=44.6484375&south=46.01222384063236&west=-180&keyword=&lang=fr&min=1&max=10&sort=title'
-	);
-	return {
-		lang: params.lang,
-		results: await response.json()
-	};
-};
---
+++ src/routes/[lang]/map-browser/+page.ts
@@ -0,0 +1,14 @@
+export const load = async ({ fetch, params, url }) => {
+	console.log(url);
+	const response = await generateUrl(url.searchParams);
+	return {
+		lang: params.lang,
+		results: await response.json()
+	};
+};
+
+function generateUrl(searchParams) {
+	return fetch(
+		'https://geocore.api.geo.ca/geo?north=69.8698915662856&east=44.6484375&south=46.01222384063236&west=-180&keyword=&lang=fr&min=1&max=10&sort=title'
+	);
+}
