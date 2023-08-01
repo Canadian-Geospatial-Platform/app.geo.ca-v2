540b8c3c151fdca42f5c4877eb45cf5133b0ac6f
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-08-01 11:10:10 -0400

    server now creates a request using keywords given as query params and lang.

--- src/routes/[lang]/map-browser/+page.ts
+++ src/routes/[lang]/map-browser/+page.ts
@@ -1,14 +1,28 @@
 export const load = async ({ fetch, params, url }) => {
-	console.log(url);
-	const response = await generateUrl(url.searchParams);
+	const response = await generateUrl(url.searchParams, params.lang.split('-')[0]);
 	return {
 		lang: params.lang,
 		results: await response.json()
 	};
 };
 
-function generateUrl(searchParams) {
-	return fetch(
-		'https://geocore.api.geo.ca/geo?north=69.8698915662856&east=44.6484375&south=46.01222384063236&west=-180&keyword=&lang=fr&min=1&max=10&sort=title'
-	);
+function generateUrl(searchParams, lang) {
+	const url2 =
+		'https://geocore.api.geo.ca/geo?north=69.8698915662856&east=44.6484375&south=46.01222384063236&west=-180&keyword=&lang=fr&min=1&max=10&sort=title';
+	console.log(searchParams);
+	let url = new URL('https://geocore.api.geo.ca/geo');
+	const params = {
+		north: 69.8698915662856,
+		east: 44.6484375,
+		south: 46.01222384063236,
+		west: -180,
+		keyword: searchParams.get('filters-search-terms'),
+		lang: lang,
+		min: 1,
+		max: 10,
+		sort: 'title'
+	};
+	url.search = new URLSearchParams(params).toString();
+	console.log(url);
+	return fetch(url);
 }
