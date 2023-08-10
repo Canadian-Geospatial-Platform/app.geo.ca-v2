db1ce3324e92a7f531a64d330ff8dafa29d43ec9
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-08-10 13:37:19 -0400

    add skeleton for footer.

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import '../../app.css';
 	import Header from '$lib/components/header/header.svelte';
+	import Footer from '$lib/components/footer/footer.svelte';
 </script>
 
 <div class="container mx-auto bg-green-100">
 	<Header />
 	<slot />
+	<Footer />
 </div>
