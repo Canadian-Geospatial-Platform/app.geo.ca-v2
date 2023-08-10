39fd5de7f13831709d512be757679fd9fa44f0ee
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-08-10 11:50:26 -0400

    fix offset responsive design.

--- src/lib/components/filters/offset.svelte
+++ src/lib/components/filters/offset.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
+	$: innerWidth = 0;
 	export let pageCount = 10;
 	export let resultsPerPage = 10;
-	$: current = $page.url.searchParams.get('page-number') || 0;
+	$: current = parseInt($page.url.searchParams.get('page-number')) || 0;
+	$: numberArray = calculateScreenSize(innerWidth, current);
+
+	function calculateScreenSize(innerWidth, current) {
+		let ret = [current - 1, current, current + 1];
+		if (innerWidth > 1280) {
+			ret = [current - 3, current - 2, current - 1, current, current + 1, current + 2, current + 3];
+		} else if (innerWidth > 500) {
+			ret = [current - 2, current - 1, current, current + 1, current + 2];
+		}
+		if (ret[0] < 0) {
+			ret = ret.map((x) => x + ret[0] * -1);
+		}
+		return ret;
+	}
 
 	function navigate(pageNumber) {
 		let url = $page.url;
@@ -13,17 +28,20 @@
 	}
 </script>
 
-<div class="p-4 m-4 rounded-lg drop-shadow-lg bg-yellow-100 flex justify-center">
-	{#each Array(pageCount) as _, i}
+<svelte:window bind:innerWidth />
+<div
+	class="p-4 m-4 rounded-lg drop-shadow-lg bg-yellow-100 flex justify-center flex-nowrap overflow-hidden xl:flex"
+>
+	{#each numberArray as x}
 		<button
 			on:click={() => {
-				navigate(i);
+				navigate(x);
 			}}
 			class="w-12 h-12 m-4 rounded-full drop-shadow-lg"
-			class:bg-red-100={current == i}
-			class:bg-blue-100={current != i}
+			class:bg-red-100={current == x}
+			class:bg-blue-100={current != x}
 		>
-			<p class="align-bottom">{i + 1}</p>
+			<p class="align-bottom">{x + 1}</p>
 		</button>
 	{/each}
 </div>
