909a9e1bd582d9af212411f9bd51d0dbcadc8968
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-08-16 14:42:48 -0400

    small responsive improvements.

--- src/routes/[lang]/map-browser/+page.svelte
+++ src/routes/[lang]/map-browser/+page.svelte
@@ -12,9 +12,9 @@
 	});
 </script>
 
-<div class="p-4 m-4 rounded-lg drop-shadow-lg bg-custom-5 text-justify">
+<div class="p-4 m-4 rounded-lg drop-shadow-lg bg-custom-5">
 	<h1 class="text-3xl font-bold pb-4">{$page.data.t_title}</h1>
-	<p>{$page.data.t_description}</p>
+	<p class="text-justify">{$page.data.t_description}</p>
 </div>
 <div class="flex flex-wrap lg:flex-nowrap items-start">
 	<Filters class="shrink" />
