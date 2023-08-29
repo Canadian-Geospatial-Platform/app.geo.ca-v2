2ea44d6c62129d27742794f67e079ce31e080caa
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-08-29 14:12:19 -0400

    split microdata on catalog and record info.

---
+++ src/lib/components/microdata/organisations.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let item;
+	export let lang;
+</script>
+
+{#each item.distributor as x}
+	<span property="publisher" typeof="Organization">
+		<span property="name">{JSON.stringify(x.organisation[lang])}</span>
+		<span property="email">open-ouvert@tbs-sct.gc.ca</span>
+	</span>
+{/each}
