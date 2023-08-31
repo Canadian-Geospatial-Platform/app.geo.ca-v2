0f22e2faea14f79bcc07bb067c7f0e5f530f0616
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-08-31 13:46:47 -0400

    add email.

--- src/lib/components/microdata/organisations.svelte
+++ src/lib/components/microdata/organisations.svelte
@@ -5,7 +5,7 @@
 
 {#each item.distributor as x}
 	<span property="publisher" typeof="Organization">
-		<span property="name">{JSON.stringify(x.organisation[lang])}</span>
-		<span property="email">open-ouvert@tbs-sct.gc.ca</span>
+		<span property="name">{x.organisation[lang]}</span>
+		<span property="email">{x.email[lang]}</span>
 	</span>
 {/each}
