f231614dba2602f3050c253a5efbc0b8f54245ef
johnweng001 <zhiyong.weng2@ontario.ca>
2023-11-06 20:53:27 -0500

    use environment files

--- src/routes/[lang]/record/[uuid]/+page.server.ts
+++ src/routes/[lang]/record/[uuid]/+page.server.ts
@@ -1,18 +1,18 @@
 import type { PageServerLoad } from "../../../$types";
 
-
 export const load:PageServerLoad = async ({ fetch, params, url, cookies }) => {
-	console.log("loading data in server...");
+	console.log("loading data in server...", import.meta.env.MODE, import.meta.env.SSR);	
 	const lang = params.lang === 'en-ca' ? 'en' : 'fr';
+	const APP_API_DOMAIN_URL=import.meta.env.VITE_APP_API_DOMAIN_URL;
 	
 	// @ts-ignore
 	const fetchResult=async (id, lang)=>{
-		const idResponse= await fetch(`https://geocore.api.geo.ca/id/v2?id=${id}&lang=${lang}`);
+		const idResponse= await fetch(`${APP_API_DOMAIN_URL}/id/v2?id=${id}&lang=${lang}`);
 		const parsedIDResponse = await idResponse.json();
 		return parsedIDResponse;
 	}
 	const fetchRelated=async (id)=>{
-		const collectionsResponse = await fetch(`https://geocore.api.geo.ca/collections?id=${id}`);
+		const collectionsResponse = await fetch(`${APP_API_DOMAIN_URL}/collections?id=${id}`);
 		const parsedCollectionsResponse = await collectionsResponse.json();
 		const related = [];
 		if (parsedCollectionsResponse.parent !== null) {
@@ -31,7 +31,7 @@ export const load:PageServerLoad = async ({ fetch, params, url, cookies }) => {
 		return related;
 	}
 	const fetchAnalytics=async (id, lang)=>{	
-		const analyticResponse=await fetch(`https://geocore.api.geo.ca/analytics/10?uuid=${id}&lang=${lang}`);	
+		const analyticResponse=await fetch(`${APP_API_DOMAIN_URL}/analytics/10?uuid=${id}&lang=${lang}`);	
 		const parsedAnalyticResponse = JSON.parse(await analyticResponse.json());
 		return parsedAnalyticResponse;
 	}
