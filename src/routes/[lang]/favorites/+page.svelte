f404eb00d94b68234dd5722e6de743baf5a5f7a1
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-11-16 14:27:38 -0500

    remove filters from favorites section. this might be added again at a later date.

--- src/routes/[lang]/favorites/+page.svelte
+++ src/routes/[lang]/favorites/+page.svelte
@@ -11,12 +11,7 @@
 
 <h1 class="text-3xl p-2 m-2">{$page.data.t_title}</h1>
 {#if $page.data.signedIn}
-	<div class="grid grid-cols-12">
-		<div class="col-span-4 bg-yellow-100" />
-		<div class="col-span-8 bg-green-100">
-			<SearchResults {results} />
-		</div>
-	</div>
+	<SearchResults {results} />
 {:else}
 	<p class="p-2 m-2">Please sign in to view favorites</p>
 {/if}
