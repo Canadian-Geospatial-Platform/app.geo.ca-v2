8491b8190732ee631368ff48aa4235f46b9a2d0a
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2024-03-25 11:03:58 -0400

    disable sign in for staging release.

--- packages/web-app/src/lib/utils/parse-jwt.js
+++ packages/web-app/src/lib/utils/parse-jwt.js
@@ -1,29 +1,33 @@
-import { CognitoJwtVerifier } from 'aws-jwt-verify';
-import { Config } from 'sst/node/config';
+// import { CognitoJwtVerifier } from 'aws-jwt-verify';
+// import { Config } from 'sst/node/config';
 
-const COGNITO_USERPOOL_ID = Config.COGNITO_USERPOOL_ID;
-const OIDC_CLIENT_ID = Config.OIDC_CLIENT_ID;
+// const COGNITO_USERPOOL_ID = Config.COGNITO_USERPOOL_ID;
+// const OIDC_CLIENT_ID = Config.OIDC_CLIENT_ID;
 
-const verifier = CognitoJwtVerifier.create({
-	userPoolId: COGNITO_USERPOOL_ID,
-	tokenUse: 'access',
-	clientId: OIDC_CLIENT_ID
-});
+// const verifier = CognitoJwtVerifier.create({
+// 	userPoolId: COGNITO_USERPOOL_ID,
+// 	tokenUse: 'access',
+// 	clientId: OIDC_CLIENT_ID
+// });
+
+// const getToken = async (cookies) => {
+// 	let access_token = cookies.get('access_token');
+
+// 	try {
+// 		const payload = await verifier.verify(access_token);
+// 		console.log('Token is valid.');
+// 		return { ok: true, value: payload };
+// 	} catch {
+// 		cookies.delete('access_token', { path: '/' });
+// 		cookies.delete('id_token', { path: '/' });
+// 		cookies.delete('refresh_token', { path: '/' });
+// 		console.log('Token not valid! Deleting token cookies.');
+// 		return { ok: false };
+// 	}
+// };
 
 const getToken = async (cookies) => {
-	let access_token = cookies.get('access_token');
-
-	try {
-		const payload = await verifier.verify(access_token);
-		console.log('Token is valid.');
-		return { ok: true, value: payload };
-	} catch {
-		cookies.delete('access_token', { path: '/' });
-		cookies.delete('id_token', { path: '/' });
-		cookies.delete('refresh_token', { path: '/' });
-		console.log('Token not valid! Deleting token cookies.');
-		return { ok: false };
-	}
+	return { ok: false };
 };
 
 export { getToken };
