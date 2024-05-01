aee52467009972a52129986c29278759eb01d981
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2024-05-01 16:34:21 -0400

    split result-count into it's own component.

--- packages/web-app/src/lib/components/search-results/list-header.svelte
+++ packages/web-app/src/lib/components/search-results/list-header.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
+	import ResultCount from './result-count.svelte';
 	// This component is a header bar with search result information, ordering options and presentation options.
-	$: total = $page.data.total;
-	$: start = $page.data.start;
-	$: end = $page.data.end;
 	function handleValueChange(e) {
 		let newUrl = $page.url;
 		newUrl.searchParams.set('sort', e.target.value);
@@ -13,9 +11,7 @@
 </script>
 
 <div class="flex flex-row gap-3">
-	<p class="grow my-auto">
-		{start} - {end} of {total} datasets
-	</p>
+	<ResultCount />
 	<form>
 		<div class="button-2">
 			Sort By -
---
+++ packages/web-app/src/lib/components/search-results/result-count.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	$: total = $page.data.total;
+	$: start = $page.data.start;
+	$: end = $page.data.end;
+</script>
+
+<p class="grow my-auto">
+	{start} - {end} of {total} datasets
+</p>
