033c19d6a6db1f930a222768ca2f78b4757d45fe
leah <leah@pygeoapi-dep.myguest.virtualbox.org>
2024-06-19 16:02:06 -0700

    Adding keywords and map preview components to record page

--- packages/web-app/src/lib/components/record/record.svelte
+++ packages/web-app/src/lib/components/record/record.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import Description from './description.svelte';
 	import Metadata from './metadata.svelte';
+  import MapPreview from './map-preview.svelte';
+  import Keywords from './keywords.svelte';
 </script>
 
 <div class="py-10 flex flex-col space-y-3">
   <Description />
   <Metadata />
+  <MapPreview />
+  <Keywords />
 </div>
--- packages/web-app/src/routes/[lang]/map-browser/record/[uuid]/+page.svelte
+++ packages/web-app/src/routes/[lang]/map-browser/record/[uuid]/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+	import { tick } from 'svelte';
+	import { afterNavigate } from '$app/navigation';
 	import Record from '$lib/components/record/record.svelte';
 	import { page } from '$app/stores';
+
+	afterNavigate(async () => {
+		try {
+			await tick();
+			cgpv.init();
+		} catch (e) {
+			console.warn('Error initialising cgpv.', e);
+		}
+	});
 </script>
 
 <Record data={$page.data} />
