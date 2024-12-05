9643b4ede2f8162364d18ac9b878db04f10743fb
LBercovitch <leah.bercovitch@nrcan-rncan.gc.ca>
2024-12-05 07:40:33 +0000

    small screen record page resources section

--- packages/web-app/src/lib/components/tab-card/tab-card.svelte
+++ packages/web-app/src/lib/components/tab-card/tab-card.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+  import { page } from "$app/stores";
   import type { ComponentType } from "svelte";
   import Card from "$lib/components/card/card.svelte";
+  import SelectCustomized from '$lib/components/select-customized/select-customized.svelte';
 
   type Tab = {
     label: string;
     // Id used to determine which tab is active
-    tabId: number;
+    value: number;
     component: ComponentType;
   };
 
@@ -13,69 +15,75 @@
 
   export let tabContentArray: Tabs;
 
-  // Set the first tab to active by default
-  $: activeTabId = tabContentArray[0].tabId;
-  $: activeTab = tabContentArray.find((x: Tab) => x.tabId == activeTabId);
-  $: title = activeTab ? activeTab.label : "";
-  $: open = false;
+  /******************* Translations *******************/
+  const translations = $page.data.t;
+  const selectCategory = translations?.selectCategory ? translations["selectCategory"] : "Select Category";
+  const resources = translations?.resources ? translations["resources"] : "Resources";
 
-  function handleTabClick(tabId: number) {
-    activeTabId = tabId;
+  // Set the first tab to active by default
+  let activeTabId = tabContentArray[0].value;
+  let activeTab = tabContentArray.find((x: Tab) => x.value == activeTabId);
+  $: title = activeTab ? activeTab.label : "";
+
+  function handleTabClick(tab: Tab) {
+    activeTab = tab;
+    activeTabId = tab.value;
   };
 
-  function handleDropdownClick() {
-    open = !open;
+  function handleDropdownClick(event: CustomEvent) {
+    handleTabClick(event.detail);
   }
+
 </script>
 
 <div class="font-custom-style-body-1">
   <div class="flex flex-wrap justify-between">
     <!-- Using a max width here ensures the title wraps instead of the menu tab for small screens -->
     <!-- Usig a min width for large screens ensures the flex wrap doesn't toggle when the tabs are clicked -->
-    <h2 class="font-custom-style-h2 mx-5 md:mx-0 max-w-tabbed-title lg:max-w-none lg:min-w-80 mb-1">
+    <h2 class="hidden lg:flex font-custom-style-h2 mx-5 md:mx-0 max-w-tabbed-title lg:max-w-none lg:min-w-80 mb-1">
       {title}
+    </h2>
+    <h2 class="lg:hidden font-custom-style-h2 mx-5 md:mx-0 mb-1">
+      {resources}
     </h2>
     <div class="flex mr-6 space-x-3 self-end">
       <!-------------- Tabs for large screens -------------->
       {#each tabContentArray as tab}
         <button 
           class="hidden lg:flex items-center h-9 px-5 font-custom-style-body-3 bg-custom-5 border-b-[0.1875rem] border-custom-16"
-          class:active={tab.tabId == activeTabId}
-          on:click={() => handleTabClick(tab.tabId)}
-        >
-          {tab.label}
-        </button>
-      {/each}
-      <!-------------- Dropdown for smaller screens -------------->
-      <button
-        class="flex flex-col lg:hidden justify-center items-center py-3 px-5 space-y-[0.3125rem] bg-custom-5"
-        class:open={open}
-        on:click={handleDropdownClick}
-      >
-        <div class="bar bar1"></div>
-        <div class="bar bar2"></div>
-        <div class="bar bar3"></div>
-      </button>
-    </div>
-    <div
-      class="flex flex-col lg:hidden w-full bg-custom-5 py-6 space-y-3 border-b-[0.1875rem] border-custom-16"
-      class:hidden={!open}
-    >
-      {#each tabContentArray as tab}
-        <button 
-          class="flex lg:hidden justify-start mx-6 font-custom-style-body-3 bg-custom-1 p-3"
-          class:active={tab.tabId == activeTabId}
-          on:click={() => handleTabClick(tab.tabId)}
+          class:active={tab.value == activeTabId}
+          on:click={() => handleTabClick(tab)}
         >
           {tab.label}
         </button>
       {/each}
     </div>
-    <!-------------- ^^ End of dropdown ^^ -------------->
+  </div>
+  <!-------------- Select for small screens -------------->
+  <div class="bg-custom-5 lg:hidden">
+    <h2 class="mx-5 font-custom-style-header-1 pt-5">
+      {selectCategory}
+    </h2>
+    <div class="mx-5 mt-2.5">
+      <SelectCustomized
+        buttonWidth='w-64'
+        optionsData={tabContentArray}
+        selectId={"select-category"}
+        buttonClasses={"button-4"}
+        dropDownColor={"#002E62"}
+        bind:selected={activeTab}
+        on:selectedChange={handleDropdownClick}
+      />
+    </div>
   </div>
   {#each tabContentArray as tabContent}
-    {#if activeTabId == tabContent.tabId}
-      <Card>
+    {#if activeTabId == tabContent.value}
+      <Card spaceBetween='space-y-3'>
+        <div class="lg:hidden">
+          <h2 class="font-open-sans font-bold text-xl">
+            {tabContent.label}
+          </h2>
+        </div>
         <svelte:component this={tabContent.component}/>
       </Card>
     {/if}
@@ -86,28 +94,5 @@
   .active {
     @apply border-b-0;
     @apply text-custom-7;
-  }
-
-  .bar {
-    @apply w-7;
-    @apply h-1;
-    @apply bg-custom-16;
-    @apply rounded;
-  }
-
-  .open .bar1 {
-    @apply origin-top-left;
-    @apply rotate-45;
-    @apply translate-x-1.5;
-  }
-
-  .open .bar2 {
-    @apply opacity-0;
-  }
-
-  .open .bar3 {
-    @apply origin-bottom-left;
-    @apply -rotate-45;
-    @apply translate-x-1.5;
   }
 </style>
