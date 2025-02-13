47142b89018b5ec03518e143f581f877caee3f73
LBercovitch <leah.bercovitch@nrcan-rncan.gc.ca>
2025-02-13 21:39:45 +0000

    adding microdata component to record page

--- packages/web-app/src/lib/components/record/record.svelte
+++ packages/web-app/src/lib/components/record/record.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { navigating } from '$app/stores';
+  import { navigating, page } from '$app/stores';
+  import MicroData from '$lib/components/microdata/microdata.svelte';
   import LoadingMask from '$lib/components/loading-mask/loading-mask.svelte';
 	import Description from './description.svelte';
 	import Metadata from './metadata.svelte';
@@ -8,6 +9,8 @@
   import SimilarProducts from './similar-products.svelte';
   import Tabbed from './tabbed/tabbed.svelte';
 </script>
+
+<MicroData item={$page.data.item_v2} />
 
 {#if $navigating}
   <LoadingMask classes="fixed left-0 top-0" />
