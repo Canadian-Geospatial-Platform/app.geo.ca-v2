1152a4b963cb82c9c2f10ac871553d7fad32cb13
LBercovitch <leah.bercovitch@nrcan-rncan.gc.ca>
2025-03-21 00:33:52 +0000

    split filter orgs between federal and provincial

--- packages/web-app/src/lib/components/search-results/filters/organizations.svelte
+++ packages/web-app/src/lib/components/search-results/filters/organizations.svelte
@@ -8,6 +8,12 @@
   const organizations = filters.find((x: Filter) => x.section === 'org');
   let checkedStates = $state({});
 
+  // Labels
+  const federal = $page.lang == 'fr-ca' ?
+    'Organisations - Fédérales' : 'Organizations - Federal';
+  const provincial = $page.lang == 'fr-ca' ?
+    'Organisations - Provinciales/Territoriales' : 'Organizations - Provincial/Territorial';
+
   // Reset filters based on current URL search params
   export function resetFilters() {
     let orgKey = $page.url.searchParams.get('org');
@@ -24,19 +30,42 @@
   }
 </script>
 
+<!-- Federal -->
 <h3 class="font-custom-style-h3">
-  {organizations?.label}
+  {federal}
 </h3>
 <div class="space-y-[1.125rem] lg:columns-2">
   {#each organizations.filterList as filterListItem}
-    <CheckboxCustomized
-      checkboxId={organizations.section + "-" + filterListItem.value}
-      checkboxName={organizations.section + "-" + filterListItem.value}
-      checkboxLabel={filterListItem.label}
-      checked={checkedStates[filterListItem.value] || false}
-      checkedStateChange={
-        (event) => checkedStates[filterListItem.value] = event.target.checked
-      }
-    />
+    {#if filterListItem.subCategory == 'fed'}
+      <CheckboxCustomized
+        checkboxId={organizations.section + "-" + filterListItem.value}
+        checkboxName={organizations.section + "-" + filterListItem.value}
+        checkboxLabel={filterListItem.label}
+        checked={checkedStates[filterListItem.value] || false}
+        checkedStateChange={
+          (event) => checkedStates[filterListItem.value] = event.target.checked
+        }
+      />
+    {/if}
+  {/each}
+</div>
+
+<!-- Provincial/Territorial -->
+<h3 class="font-custom-style-h3">
+  {provincial}
+</h3>
+<div class="space-y-[1.125rem] lg:columns-2">
+  {#each organizations.filterList as filterListItem}
+    {#if filterListItem.subCategory == 'prov'}
+      <CheckboxCustomized
+        checkboxId={organizations.section + "-" + filterListItem.value}
+        checkboxName={organizations.section + "-" + filterListItem.value}
+        checkboxLabel={filterListItem.label}
+        checked={checkedStates[filterListItem.value] || false}
+        checkedStateChange={
+          (event) => checkedStates[filterListItem.value] = event.target.checked
+        }
+      />
+    {/if}
   {/each}
 </div>
