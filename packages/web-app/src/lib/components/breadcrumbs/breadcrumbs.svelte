8664b1ad9066b779dcb26d4232b2cddfa1dd1227
LBercovitch <leah.bercovitch@nrcan-rncan.gc.ca>
2025-03-26 01:25:15 +0000

    last breadcrumbs item no longer contains a link

--- packages/web-app/src/lib/components/breadcrumbs/breadcrumbs.svelte
+++ packages/web-app/src/lib/components/breadcrumbs/breadcrumbs.svelte
@@ -1,24 +1,37 @@
 <script lang="ts">
   import { page } from '$app/stores';
 
-  let geoCaUrl = $derived($page.data.lang == 'en-ca' ?
-    'https://geo.ca/home' : 'https://geo.ca/fr/accueil/index.html');
-  let appGeoCaUrl = $derived('/' + $page.data.lang + '/map-browser');
+  const geoCaUrl = $derived($page.data.lang == 'fr-ca' ?
+    'https://geo.ca/fr/accueil/index.html' : 'https://geo.ca/home');
+  const homeLabel = $derived($page.data.lang == 'fr-ca' ? 'Accueil' : 'Home');
+
+  const appGeoCaUrl = $derived('/' + $page.data.lang + '/map-browser');
+  const searchLabel = $derived($page.data.lang == 'fr-ca' ? 'Recherche' : 'Search');
+
+  const title2 = $derived($page.data.t_title_2);
+  const title3 = $derived($page.data.t_title_3);
+
+  let breadcrumbs = $derived([
+    {"text": homeLabel, "href": geoCaUrl},
+    {"text": searchLabel, "href": appGeoCaUrl},
+    ...(title2 ? [title2] : []),
+    ...(title3 ? [title3] : []),
+  ]);
 </script>
 
-<ol class="flex flex-wrap px-5 md:px-0 gap-2 text-custom-8">
-  <li><a href={geoCaUrl}>Home</a></li>
-  <li>/ <a href={appGeoCaUrl}>Search</a></li>
-  {#if $page.data.t_title_2}
-    <li>/ <a href={$page.data.t_title_2.href}>{$page.data.t_title_2.text}</a></li>
-  {/if}
-  {#if $page.data.t_title_3}
-    <li>/ <a href={$page.data.t_title_3.href}>{$page.data.t_title_3.text}</a></li>
-  {/if}
+<ol class="flex flex-wrap px-5 md:px-0 gap-1">
+  {#each breadcrumbs as breadcrumb, i}
+    {#if i < breadcrumbs.length - 1}
+      <li><a href={breadcrumb.href}>{breadcrumb.text}</a> /</li>
+    {:else}
+      <li>{breadcrumb.text}</li>
+    {/if}
+  {/each}
 </ol>
 
 <style>
   a {
     @apply underline;
+    @apply text-custom-8;
   }
 </style>
