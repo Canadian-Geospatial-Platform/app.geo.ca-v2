e06987f666bc3636047e725f61782f21cf0e397b
EC2 Default User <ec2-user@ip-10-68-178-17.ca-central-1.compute.internal>
2025-06-24 16:04:26 +0000

    temporary fix for similar record description

---
+++ packages/web-app/src/routes/[lang]/map-browser/record/[uuid]/+server.ts
@@ -0,0 +1,46 @@
+import { json } from '@sveltejs/kit';
+
+export async function POST({ request }) {
+  const { ids, lang } = await request.json();
+
+  if (!Array.isArray(ids)) {
+    return json({ error: 'Invalid IDs' }, { status: 400 });
+  }
+
+  const records = await getRecordsByIds(ids, lang);
+
+  return json(records);
+}
+
+function getRecord(id, lang) {
+	const url = new URL('https://geocore.api.geo.ca/id/v2');
+	const params = {
+		id: id,
+		lang: lang.split('-')[0]
+	};
+	url.search = new URLSearchParams(params).toString();
+	return fetch(url);
+}
+
+async function getRecordsByIds(idIterator, lang) {
+	let promises = [];
+
+	for (const id of idIterator) {
+		promises.push(getRecord(id, lang));
+	}
+
+	const results = await Promise.all(promises);
+	const values = [...results];
+
+	let ret = await Promise.all(
+		values.map(async (v) => {
+			try {
+				const contents = await v.json();
+				if (contents?.body?.Items[0]) return contents.body.Items[0];
+			} catch {
+				(error) => console.log(error);
+			}
+		})
+	);
+	return ret;
+}
