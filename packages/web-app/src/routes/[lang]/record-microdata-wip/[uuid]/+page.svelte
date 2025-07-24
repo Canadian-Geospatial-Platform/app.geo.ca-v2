ba20c2d4344b7e02237c955cf6fe4db66505021d
Pascal Vautour <vautour.pascal@gmail.com>
2025-07-24 13:47:22 -0400

    remove old microdata proof of concept page.

--- packages/web-app/src/routes/[lang]/record-microdata-wip/[uuid]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import MicroData from '$lib/components/microdata/microdata.svelte';
-	import { page } from '$app/stores';
-</script>
-
-<MicroData item={$page.data.item_v2} />
-<h1>This page will display a detailed view of a specific result.</h1>
-<p>The UUID of the result currently searched for is: {$page.data.uuid}.</p>
-<p>The language: {$page.data.lang}.</p>
