f0dd683243ffc6c5002d73eecba9c54d9cf74bee
Bo Lu <bo.lu@canada.ca>
2026-04-30 16:08:18 +0000

    Updates email address to geoinfo@nrcan-rncan.gc.ca site-wide #82

--- packages/web-app/src/lib/components/feedback/feedback.svelte
+++ packages/web-app/src/lib/components/feedback/feedback.svelte
@@ -14,14 +14,14 @@
       <p>
         GEO.ca s’engage à favoriser un dialogue ouvert et à renforcer la communauté autour des enjeux et sujets liées à la localisation qui
         vous intéressent.
-        <br /><a class="underline hover:no-underline text-custom-16" href="mailto:geo@nrcan-rncan.gc.ca"
+        <br /><a class="underline hover:no-underline text-custom-16" href="mailto:geoinfo@nrcan-rncan.gc.ca?subject=app.geo.ca"
           >Faites-nous part de vos commentaires</a
         >
       </p>
     {:else}
       <p>
         GEO.ca is committed to open dialogue and community building around location-based issues and topics that matter to you.
-        <br /><a class="underline hover:no-underline text-custom-16" href="mailto:geo@nrcan-rncan.gc.ca">Please send us your feedback</a>
+        <br /><a class="underline hover:no-underline text-custom-16" href="mailto:geoinfo@nrcan-rncan.gc.ca?subject=app.geo.ca">Please send us your feedback</a>
       </p>
     {/if}
   </div>
